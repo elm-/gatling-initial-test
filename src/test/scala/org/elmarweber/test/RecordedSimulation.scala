13bacf7a13ee630f2bfae76ab9e3e86ceadc6acb
Elmar Weber <info@elmarweber.org>
2013-11-07 14:07:46 +0100

    - created gatling test

---
+++ src/test/scala/org/elmarweber/test/RecordedSimulation.scala
@@ -0,0 +1,52 @@
+package org.elmarweber.test
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
+import assertions._
+import org.apache.commons.lang.RandomStringUtils
+
+class RecordedSimulation extends Simulation {
+	val httpConf = httpConfig
+			.baseURL("http://localhost:8080")
+			.acceptHeader("application/json")
+
+
+	val headers = Map(
+			"Cache-Control" -> """no-cache""",
+			"Content-Type" -> """application/json; charset=UTF-8""",
+			"Pragma" -> """no-cache"""
+	)
+
+  val userDataFeeder = new Feeder[String] {
+    def hasNext = true
+
+    def next() = Map(
+      "username" -> RandomStringUtils.randomAlphanumeric(10),
+      "displayName" -> RandomStringUtils.randomAlphanumeric(20),
+      "apiKey" -> RandomStringUtils.randomAlphanumeric(64))
+  }
+
+	val scn = scenario("Simple Create and Get")
+    .during(30 seconds) {
+      feed(userDataFeeder)
+      .exec(http("create user")
+            .post("/user/")
+            .headers(headers)
+              .body("""{
+                      |    "username": "${username}",
+                      |    "displayName": "${displayName}",
+                      |    "apiKey": "${apiKey}"
+                      |}""".stripMargin)
+            .check(status.is(200))
+        )
+      .exec(http("get")
+            .get("/user/${username}")
+            .check(status.is(200))
+      )
+    }
+
+	setUp(scn.users(8).ramp(16).protocolConfig(httpConf))
+}
